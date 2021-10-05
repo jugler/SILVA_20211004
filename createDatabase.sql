ea349a08cc5f5bec175855e6c29ebdef396a1f91
Jesus Silva <jugler@gmail.com>
2021-10-04 21:33:40 -0700

    database schema

---
+++ createDatabase.sql
@@ -0,0 +1,8 @@
+CREATE DATABASE youtubez_data
+    WITH 
+    OWNER = newuser
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
